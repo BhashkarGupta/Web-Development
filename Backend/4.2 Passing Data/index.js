f9081df0905459cfe28b12df562f5c788c47b9f9
Bhashkar Gupta <bhashkar.spi@gmail.com>
2023-11-16 01:33:30 +0530

    updated EJS module

--- Backend/4.2 Passing Data/index.js
+++ Backend/4.2 Passing Data/index.js
@@ -2,13 +2,22 @@ import express from "express";
 import bodyParser from "body-parser";
 
 const app = express();
-const port = 3000;
+const port = 2100;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {});
+function letterCount(fName, lName){
+  var sum = fName.length + lName.length;
+  return sum;
+}
 
-app.post("/submit", (req, res) => {});
+app.get("/", (req, res) => {
+  res.render("index.ejs");
+});
+
+app.post("/submit", (req, res) => {
+  res.render("index.ejs", { count: letterCount(req.body.fName, req.body.lName) });
+});
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);

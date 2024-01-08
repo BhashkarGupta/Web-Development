ad7d792fd0cdd758bd76b1d8535517c58e2fce83
Bhashkar Gupta <bhashkar.spi@gmail.com>
2024-01-08 13:56:52 +0530

    updated QR code generation task

--- Backend/2.2 Native Modules/index.mjs
+++ Backend/2.2 Native Modules/index.mjs
@@ -3,13 +3,12 @@ import { writeFile, readFile } from 'node:fs';
 const data = 'Hello Node.js'; // Just a plain string
 
 writeFile('message.txt', data, (err) => {
-    if (err) throw err;
+    if (err) throw err; // if any error happens, it will stop the execution of callback function and next line will not execute
     console.log('The file has been saved!');
 });
    
 // Now, read the file
 readFile('message.txt', 'utf8', (err, fileData) => {
-    if (err) throw err;
+    if (err) throw err; // if any error happens, it will stop the execution of callback function and next line will not execute
     console.log('File content:', fileData);
-    
 });
--- Backend/2.4 QR Code Project/index.js
+++ Backend/2.4 QR Code Project/index.js
@@ -3,3 +3,32 @@
 2. Use the qr-image npm package to turn the user entered URL into a QR code image.
 3. Create a txt file to save the user input using the native fs node module.
 */
+import inquirer from 'inquirer';
+import {writeFile} from 'node:fs';
+import qr from 'qr-image';
+import fs from 'fs';
+
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'url',
+            message: 'Please input the URL: ',
+        },
+    ])
+    .then((answers) => {
+        writeFile('URL.txt', answers.url, (err) => {
+            if(err) throw err;
+        });
+        console.log('file saved sucessfully');
+        const qrContent = qr.image(answers.url, {type: 'svg'});
+        qrContent.pipe(fs.createWriteStream('qr.svg'));
+    })
+    .catch((error) => {
+        if (error.isTtyError) {
+            console.log("Prompt couldn't be rendered in the current environment");
+        } else {
+            console.log("Something else went wrong");
+        }
+    });
+
